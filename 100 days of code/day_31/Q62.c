7235c3dfb3d7c295b58797d942aea8bf187f181b
krishna som <krishnarajput36890@gmail.com>
2025-09-27 09:02:18 +0530

    Day 31 got updated

--- 100 days of code/day_31/Q62.c
+++ 100 days of code/day_31/Q62.c
@@ -1,26 +1,18 @@
-//Q61: Search for an element in an array using linear search.
+//Q62: Reverse an array without taking extra space.
 
 /*
 Sample Test Cases:
 Input 1:
-5
-1 2 3 4 5
-3
-Output 1:
-Found at index 2
-
-Input 2:
 4
-10 20 30 40
-25
-Output 2:
--1
+1 2 3 4
+Output 1:
+4 3 2 1
 
 */
 #include <stdio.h>
 
 int main() {
-    int counter,num,posi,tofind;
+    int counter,num;
     printf("Enter the number of elements you want in your array : ");
     scanf("%d",&num);
     int arr[num+1];
@@ -28,12 +20,9 @@ int main() {
         printf("Enter the number : ");
         scanf("%d",&arr[counter]);
     }
-    printf("\nEnter the number to find : ");
-    scanf("%d",&tofind);
-    for(counter=0;counter<num;++counter){
-        if(arr[counter]==tofind){
-            printf("Found at index  : %d \n",counter);
-        }
+    printf("\nReversed array : ");
+    for(counter=num-1;counter>=0;--counter){
+        printf("%d ",arr[counter]);
     }
     return 0;
 }
