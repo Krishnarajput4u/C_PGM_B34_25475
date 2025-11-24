0a69ea48179e9712d2badd6f92ef26f4b8251582
krishna som <krishnarajput36890@gmail.com>
2025-11-24 09:31:24 +0530

    Corrected Day 79

--- 100 days of code/day_79/Q129.c
+++ 100 days of code/day_79/Q129.c
@@ -6,12 +6,14 @@ int main(){
         printf("Error opening file.\n");
         return 1;
     }
-    int number, sum = 0;
+    int number, sum = 0;int counter=0;
     while(fscanf(file, "%d", &number) == 1){
         sum += number;
+        counter++;
     }
+    float average = (float)sum / counter;
     fclose(file);
     printf("The sum of the numbers in the file is: %d\n", sum);
-    printf("Average of the numbers in the file is: %.2f\n", (float)sum / 10);
+    printf("Average of the numbers in the file is: %.2f\n", average);
     return 0;
 }
